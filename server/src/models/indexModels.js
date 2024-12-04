0c1742bcd13ef6145fbe000446a5eb74f06ec79a
blackcave0 <i3intel@protonmail.com>
2024-12-05 04:07:34 +0530

    in this file importing the all models

---
+++ server/src/models/indexModels.js
@@ -0,0 +1,11 @@
+import { Customer, Admin , DeliveryPartner } from "./userModel.js";
+import Branch from "./branchModel.js";
+
+// import the category model and product model 
+import { Category } from "./category.js";
+import { Product } from "./products.js";
+import { Order } from "./order.js";
+import { Counter } from "./counter.js";
+
+
+export { Customer, Admin, DeliveryPartner, Branch, Category, Product, Order, Counter };
